6830fa2281b5207b7e9eb66944bd363ed5dc5c88
Jennifer <Jreyes101064@ppsc.edu.mx>
2023-05-09 16:09:59 -0600

    precios y descripciones

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-//
+const carrito = document.querySelector('#carrito');
+const listaProductos = document.querySelector('#product-items');
+const contenedor = document.querySelector('#articulos-carrito tbody');
+const botonVaciar = document.querySelector('#vaciar-carrito')
+
+let elementosCarrito = [];
+
